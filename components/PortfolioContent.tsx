11919c0f237eb9511af780595e447005da9e29d8
saw <saw>
2025-10-29 00:43:53 +1300

    Update layout

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -1,30 +1,31 @@
 import { AboutSection } from "./sections/AboutSection";
 import { AchievementsSection } from "./sections/AchievementsSection";
-import { BlogSection } from "./sections/BlogSection";
-import { CertificationsSection } from "./sections/CertificationsSection";
+// import { BlogSection } from "./sections/BlogSection";
+// import { CertificationsSection } from "./sections/CertificationsSection";
 import { ContactSection } from "./sections/ContactSection";
 import { EducationSection } from "./sections/EducationSection";
 import { ExperienceSection } from "./sections/ExperienceSection";
 import HeroSection from "./sections/HeroSection";
 import { ProjectsSection } from "./sections/ProjectsSection";
-import { ServicesSection } from "./sections/ServicesSection";
+// import { ServicesSection } from "./sections/ServicesSection";
 import { SkillsSection } from "./sections/SkillsSection";
-import { TestimonialsSection } from "./sections/TestimonialsSection";
+// import { TestimonialsSection } from "./sections/TestimonialsSection";
 
 async function PortfolioContent() {
   return (
     <>
       <HeroSection />
       <AboutSection />
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
+        <ProjectsSection />
       <SkillsSection />
       <ExperienceSection />
       <EducationSection />
-      <ProjectsSection />
-      <CertificationsSection />
+    
+      {/* <CertificationsSection /> */}
       <AchievementsSection />
-      <ServicesSection />
-      <BlogSection />
+      {/* <ServicesSection /> */}
+      {/* <BlogSection /> */}
       <ContactSection />
     </>
   );
